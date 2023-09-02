e03bd8456fa052f27f834b23cdba0beb87d1ffbe
Ivnmendes <ivanmendes2005@gmail.com>
2023-09-02 11:30:27 -0300

    Trabalho v2

---
+++ labProg1/h1.trabalho_if-else/README.md
@@ -0,0 +1,127 @@
+# Programa com if-else
+
+Você foi contratado pela empresa ZOXY para desenvolver um sistema para efetuar cálculos diversos utilizados pelos funcionários da empresa no dia a dia. Para auxiliar nessa tarefa, foi disponibilizada uma especificação detalhada de todos os menus e submenus a serem desenvolvidos, bem como as ações a serem executadas. Para facilitar a compreensão, a especificação foi dividida em etapas, cada qual correspondendo a uma funcionalidade.
+
+**Etapa 1**: Criação do menu inicial do programa. No cabeçalho do menu, deve ser exibido o nome da empresa (‘ZOXY’) dentro de um retângulo demarcado por asteriscos (*). A seguir, devem ser exibidas para o usuário quatro opções e solicitada uma entrada em formato caractere, conforme segue:
+
+```c
+Menu Inicial
+a. Validacao de data
+b. Diferenca de horarios
+c. Conversao de temperatura
+d. Calculo de material
+>Selecione uma opcao:_
+```
+
+As quatro opções possíveis devem aceitar tanto com caracteres minúsculos quanto maiúsculos (ex.: ‘A’ ou ‘a’, etc.). Se o usuário informar uma opção inválida, o programa deve exibir uma mensagem de erro e encerrar imediatamente.
+
+**Etapa 2a**: Se o usuário selecionar a opção ‘a’, o programa exibe o título “A > Validação de data” e solicita três valores ao usuário, sendo eles “Dia”, “Mês” e “Ano”. Em seguida, o programa deve verificar se a data digitada é válida, considerando as seguintes premissas:
+- Janeiro, março, maio, julho, agosto, outubro e dezembro sempre possuem 31 dias e os demais meses (com exceção de fevereiro) sempre possuem 30 dias; e
+- Fevereiro possui 28 dias, exceto em anos bissextos, em que possui 29 dias. Um ano é bissexto se ele for divisível por 400 ou se ele for divisível por 4 e não por 100.
+
+Se, por qualquer um dos motivos acima, a data digitada for inválida, o programa deve exibir uma mensagem de erro e encerrar imediatamente. Caso a data seja válida, o programa exibe um menu contendo três opções para a formatação da data e, com base na opção informada, formata a data e a exibe de acordo. Por fim, exibe uma mensagem de finalização (* fim *)
+
+```c
+A > Validacao de data
+>Dia:29
+>Mes:02
+>Ano:2020
+1. DD/MM/YYYY
+2. YYYY-MM-DD
+3. DD de [Mes] de YYYY
+>Selecione uma opcao:_
+<output
+* fim *
+```
+
+No exemplo, o **output** varia de acordo com a opção selecionada, sendo:
+
+- Opção 1: **29/02/2020**
+- Opção 2: **2020-02-29**
+- Opção 3: **29 de fevereiro de 2020**
+- Para quaisquer outras opções, o programa exibe mensagem de erro e encerra imediatamente.
+
+**Etapa 2b**: Se o usuário selecionar a opção ‘b’, o programa exibe o título “B > Diferença de horários” e solicita quatro valores ao usuário, sendo eles (em ordem): “Hora h1”, “Minuto m1”, “Hora h2”, “Minuto m2”. Considere que os inputs estão no mesmo fuso horário e em formato 24h. Em seguida, o programa calcula e exibe a diferença entre os horários informados (h1:m1 e h2:m2) de acordo com a opção de visualização selecionada pelo usuário (“Padrão”, “Minutos”, “Segundos” ou “Milissegundos”). **Atenção**: se o segundo horário informado (h2:m2) for menor ou igual ao primeiro horário (h1:m1), considera-se que eles ocorrem em dias diferentes (isso é, o segundo horário ocorre no dia d+1)
+
+```c
+B > Diferenca de horarios
+>Hora h1:15
+>Minuto m1:30
+>Hora h2:14
+>Minuto m2:00
+1. Padrao
+2. Minutos
+3. Segundos
+4. Milisegundos
+>Selecione uma opcao:_
+<Diferenca: output
+* fim *
+```
+
+No exemplo, o **output** varia de acordo com a opção selecionada, sendo:
+
+- Opção 1: **22 horas e 30 minutos**
+- Opção 2: **1350m**
+- Opção 3: **81000s**
+- Opção 4: **81000000ms**
+- Para quaisquer outras opções, o programa exibe mensagem de erro e encerra imediatamente.
+
+**Etapa 2c**: Se o usuário selecionar a opção ‘c’, o programa exibe o título “C > Conversão de temperatura” e lê um valor real do usuário equivalente a uma temperatura em escala Celsius. Após, solicita uma escala para conversão (“Fahrenheit”, “Kelvin” ou “Rankine”), calcula e exibe a temperatura na escala escolhida. Considere as seguinte equivalências, onde ºC é uma temperatura em Celsius, ºF em Fahrenheit, K em Kelvin e ºR na escala Rankine:
+
+- $(°C \dot \frac{9}{5}) + 32 = °F$
+- $(°C + 273.15 = K$
+- $(°C \dot \frac{9}{5}) + 491.67 = °R$
+
+```c
+C > Validacao de data
+>Temperatura (°C): 37.5
+1. Fahrenheit
+2. Kelvin
+3. Rankine
+>Selecione uma opcao:_
+<output
+* fim *
+```
+
+No exemplo, o **output** varia de acordo com a opção selecionada, sendo:
+
+- Opção 1: **37.5°C <-> 99.5°F**
+- Opção 2: **37.5°C <-> 310.65K**
+- Opção 3: **37.5°C <-> 559.17°R**
+- Para quaisquer outras opções, o programa exibe mensagem de erro e encerra imediatamente.
+
+**Etapa 2d**: Se o usuário selecionar a opção ‘d’, o programa exibe o título “D > Cálculo de material” e lê dois valores reais equivalentes à altura e ao comprimento (em metros) de uma parede qualquer. Em seguida, solicita ao usuário qual tipo de tijolo ele deseja utilizar como base para o cálculo e retorna o número de tijolos a serem necessários (arredondado para cima, quando aplicável) para cobrir a área dada pela altura e comprimento informados e o valor final da compra (arredondado para duas casas decimais). Considere os seguintes tipos de tijolos e suas respectivas dimensões (altura x comprimento) e o valor do milheiro de tijolos:
+
+- 6 furos: 14cm * 19cm, R$ 400,00/milheiro
+- 8 furos: 19cm * 19cm, R$ 780,50/milheiro
+- 9 furos: 19cm * 29cm, R$ 899,99/milheiro
+
+```c
+C > Calculo de material
+>Altura (m): 3.5
+>Comprimento (m): 2.5
+1. 6 furos
+2. 8 furos
+3. 9 furos
+>Selecione uma opcao:_
+<output
+* fim *
+```
+
+No exemplo, o **output** varia de acordo com a opção selecionada, sendo:
+
+- Opção 1: **329 unidades (R$ 131.60)**
+- Opção 2: **243 unidades (R$ 189.66)**
+- Opção 3: **159 unidades (R$ 143.10)**
+- Para quaisquer outras opções, o programa exibe mensagem de erro e encerra imediatamente.
+
+Orientações:
+- **A interpretação da especificação do trabalho faz parte da avaliação**;
+- O trabalho é individual;
+- **Além do conteúdo de programação sequencial (entrada e saída, expressões, variáveis), somente pode ser usado o comando de seleção IF-ELSE**;
+- O código deve estar organizado, indentado e obedecer criteriosamente à
+especificação;
+- Certifique-se de compilar e testar exaustivamente o programa antes do envio;
+- Deve ser entregue **apenas o arquivo .c**, cujo nome deve ser: nomeAluno_ifelse.c (ex. mariana_ifelse.c). Trabalhos que não obedecerem essa regra serão desconsiderados;
+- Não serão aceitos trabalhos entregues fora da plataforma Moodle;
+- Trabalhos total ou parcialmente copiados receberão nota zero.
--- labProg1/h1.trabalho_if-else/program.c
+++ labProg1/h1.trabalho_if-else/program.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 /*
  * Ivan Mendes
  */
@@ -31,7 +31,7 @@ int main()
 
     switch (op) {
         case 'a': case 'A':
-            printf("Validacao de data\n");
+            printf("\nA > Validacao de data\n");
             printf("Dia: ");
             scanf("%d", &dia);
             printf("Mês: ");
@@ -39,75 +39,79 @@ int main()
             printf("Ano: ");
             scanf("%d", &ano);
 
-            switch (mes) {
-                case 2:
-                    dataValida = ((dia <= 28) || (dia <= 29 && (ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0))) ? 't' : 'f';
-                    break;
-                case 1: case 3: case 5: case 6: case 8: case 10: case 12:
-                    dataValida = (dia<= 31) ? 't' : 'f';
-                    break;
-                case 4: case 7: case 9: case 11:
-                    dataValida = (dia<= 30) ? 't' : 'f';
-                    break;
-                default:
-                    printf("Data invalida!");
-                    break;
-            }
-
-            if(dataValida == 't') {
-                printf(
-                    "1. DD/MM/YYYY\n"
-                    "2. YYYY-MM-DD\n"
-                    "3. DD de [MES] de YYYY\n"
-                    "Selecione uma opcao: "
-                    );
-
-                scanf("%d", &opFormatacao);
-
-                switch (opFormatacao) {
-                    case 1:
-                        printf("%0*d/%0*d/%d", dia, mes, ano);
-                        break;
+            if(dia >= 1 && ano >= 0) {
+                switch (mes) {
                     case 2:
-                        printf("%d-%0*d-%0*d", ano, mes, dia);
+                        dataValida = ((dia <= 28) || (dia <= 29 && (ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0))) ? 't' : 'f';
                         break;
-                    case 3:
-                        printf("%d de ", dia);
-                        if(mes == 1) {
-                            printf("janeiro");
-                        } else if (mes == 2) {
-                            printf("fevereiro");
-                        } else if (mes == 3) {
-                            printf("marco");
-                        } else if (mes == 4) {
-                            printf("abril");
-                        } else if (mes == 5) {
-                            printf("maio");
-                        } else if (mes == 6) {
-                            printf("junho");
-                        } else if (mes == 7) {
-                            printf("julho");
-                        } else if (mes == 8) {
-                            printf("agosto");
-                        } else if (mes == 9) {
-                            printf("setembro");
-                        } else if (mes == 10) {
-                            printf("outubro");
-                        } else if (mes == 11) {
-                            printf("novembro");
-                        } else {
-                            printf("dezembro");
-                        }
-                        printf(" de  %d", ano);
+                    case 1: case 3: case 5: case 6: case 8: case 10: case 12:
+                        dataValida = (dia<= 31) ? 't' : 'f';
                         break;
-                    default:
-                        printf("Opcao Invalida!");
+                    case 4: case 7: case 9: case 11:
+                        dataValida = (dia<= 30) ? 't' : 'f';
                         break;
                 }
+
+                if(dataValida == 't') {
+                    printf(
+                        "1. DD/MM/YYYY\n"
+                        "2. YYYY-MM-DD\n"
+                        "3. DD de [MES] de YYYY\n"
+                        "Selecione uma opcao: "
+                        );
+
+                    scanf("%d", &opFormatacao);
+
+                    switch (opFormatacao) {
+                        case 1:
+                            printf("%02d/%02d/%d", dia, mes, ano);
+                            break;
+                        case 2:
+                            printf("%d-%02d-%02d", ano, mes, dia);
+                            break;
+                        case 3:
+                            printf("%d de ", dia);
+                            if(mes == 1) {
+                                printf("janeiro");
+                            } else if (mes == 2) {
+                                printf("fevereiro");
+                            } else if (mes == 3) {
+                                printf("marco");
+                            } else if (mes == 4) {
+                                printf("abril");
+                            } else if (mes == 5) {
+                                printf("maio");
+                            } else if (mes == 6) {
+                                printf("junho");
+                            } else if (mes == 7) {
+                                printf("julho");
+                            } else if (mes == 8) {
+                                printf("agosto");
+                            } else if (mes == 9) {
+                                printf("setembro");
+                            } else if (mes == 10) {
+                                printf("outubro");
+                            } else if (mes == 11) {
+                                printf("novembro");
+                            } else {
+                                printf("dezembro");
+                            }
+                            printf(" de %d", ano);
+                            break;
+                        default:
+                            printf("Opcao Invalida!");
+                            break;
+                    }
+                    printf("\n* fim *");
+                } else {
+                    printf("Data invalida\n");
+                }
+            } else {
+                printf("Data invalida\n");
             }
             break;
         case 'b': case 'B':
-            printf("Diferenca de horarios\n");
+            printf("\nB > Diferenca de horarios\n");
             printf("Hora 1: ");
             scanf("%d", &hora1);
             printf("Minuto 1: ");
@@ -117,60 +121,66 @@ int main()
             printf("Minuto 2: ");
             scanf("%d", &min2);
 
-            printf(
-                    "1. Padrao\n"
-                    "2. Minutos\n"
-                    "3. Segundos\n"
-                    "4. Milisegundos\n"
-                    "Selecione uma opcao: "
-                    );
-            scanf("%d", &opCalcHora);
+            if(hora1 >= 0 && hora1 <= 24 && min1 >= 0 && min1 <= 59 && hora2 >= 0 && hora2 <= 24 && min2 >= 0 && min2 <= 59) {
+                printf(
+                        "1. Padrao\n"
+                        "2. Minutos\n"
+                        "3. Segundos\n"
+                        "4. Milisegundos\n"
+                        "Selecione uma opcao: "
+                        );
+                scanf("%d", &opCalcHora);
 
-            tTotal1 = hora1 * 3600 + min1 * 60;
-            tTotal2 = hora2 * 3600 + min2 * 60;
-                switch (opCalcHora) {
-                    case 1:
-                        if(hora1 * 60 + min1 <= hora2 * 60 + min2) {
-                            tTotal1 += 86400;
-                            printf("%d horas e %d minutos", (tTotal1 - tTotal2) / 3600, (tTotal1 - tTotal2) % 3600 / 60);
-                        } else {
-                            printf("%d horas e %d minutos", (tTotal1 - tTotal2) / 3600, (tTotal1 - tTotal2) % 3600 / 60);
-                        }
-                        break;
-                    case 2:
-                        if(hora1 * 60 + min1 <= hora2 * 60 + min2) {
-                            tTotal1 += 86400;
-                            printf("%dm", (tTotal1 - tTotal2) / 60);
-                        } else {
-                            printf("%dm", (tTotal1 - tTotal2) / 60);
-                        }
-                        break;
-                    case 3:
-                        if(hora1 * 60 + min1 <= hora2 * 60 + min2) {
-                            tTotal1 += 86400;
-                            printf("%ds", tTotal1 - tTotal2);
-                        } else {
-                            printf("%ds", tTotal1 - tTotal2);
-                        }
-                        break;
-                    case 4:
-                        if(hora1 * 60 + min1 <= hora2 * 60 + min2) {
-                            tTotal1 += 86400;
-                            printf("%dms", (tTotal1 - tTotal2) * 1000);
-                        } else {
-                            printf("%dms", (tTotal1 - tTotal2) * 1000);
-                        }
-                        break;
-                    default:
-                        printf("Opcao invalida!");
-                        break;
-                }
+                //Conversão do tempo inserido para segundos, facilitando os cálculos
+                tTotal1 = hora1 * 3600 + min1 * 60;
+                tTotal2 = hora2 * 3600 + min2 * 60;
+                    switch (opCalcHora) {
+                        case 1:
+                            if(hora1 * 60 + min1 >= hora2 * 60 + min2) {
+                                tTotal2 += 86400; //Adição de um dia (em segundos)
+                                printf("%d horas e %d minutos", (tTotal2 - tTotal1) / 3600, (tTotal2 - tTotal1) % 3600 / 60);
+                            } else {
+                                printf("%d horas e %d minutos", (tTotal2 - tTotal1) / 3600, (tTotal2 - tTotal1) % 3600 / 60);
+                            }
+                            break;
+                        case 2:
+                            if(hora1 * 60 + min1 >= hora2 * 60 + min2) {
+                                tTotal2 += 86400;
+                                printf("%dm", (tTotal2 - tTotal1) / 60);
+                            } else {
+                                printf("%dm", (tTotal2 - tTotal1) / 60);
+                            }
+                            break;
+                        case 3:
+                            if(hora1 * 60 + min1 >= hora2 * 60 + min2) {
+                                tTotal2 += 86400;
+                                printf("%ds", tTotal2 - tTotal1);
+                            } else {
+                                printf("%ds", tTotal2 - tTotal1);
+                            }
+                            break;
+                        case 4:
+                            if(hora1 * 60 + min1 >= hora2 * 60 + min2) {
+                                tTotal2 += 86400;
+                                printf("%dms", (tTotal2 - tTotal1) * 1000);
+                            } else {
+                                printf("%dms", (tTotal2 - tTotal1) * 1000);
+                            }
+                            break;
+                        default:
+                            printf("Opcao invalida!");
+                            break;
+                    }
+                    printf("\n* fim *");
+            } else {
+                printf("Insira um horario valido!");
+            }
             break;
         case 'c': case 'C':
-            printf("Diferenca de temperatura\n");
+            printf("\nC > Diferenca de temperatura\n");
             printf("Temperatura (°C): ");
-            scanf("%f", temp);
-            
+            scanf("%f", &temp);
+
             printf(
                     "1. Fahrenheit\n"
                     "2. Kelvin\n"
@@ -193,37 +203,44 @@ int main()
                     printf("Opcao invalida");
                     break;
             }
+            printf("\n* fim *");
             break;
         case 'd': case 'D':
-            printf("Calculo de material\n");
+            printf("\nD > Calculo de material\n");
             printf("Altura (m): ");
-            scanf("%f", altura);
+            scanf("%f", &altura);
             printf("Comprimento (m): ");
-            scanf("%f", comprimento);
+            scanf("%f", &comprimento);
 
-            printf(
-                    "1. 6 furos\n"
-                    "2. 8 furos\n"
-                    "3. 9 furos\n"
-                    "Selecione uma opcao: "
-                    );
-            scanf("%d", &opMaterial);
+            if(altura > 0 && comprimento > 0) {
+                printf(
+                        "1. 6 furos\n"
+                        "2. 8 furos\n"
+                        "3. 9 furos\n"
+                        "Selecione uma opcao: "
+                        );
+                scanf("%d", &opMaterial);
 
-            switch (opMaterial) {
-                case 1:
-                    tTijolos = (altura * 100 * comprimento * 100) /  (266);
-                    printf("%d unidades (R$ %.2f)", tTijolos, tTijolos * 400 / 100);
-                    break;
-                case 2:
-                    tTijolos = (altura * 100 * comprimento * 100) /  (361);
-                    printf("%d unidades (R$ %.2f)", tTijolos, tTijolos * 780.5 / 100);
-                    break;
-                case 3:
-                    tTijolos = (altura * 100 * comprimento * 100) /  (551);
-                    printf("%d unidades (R$ %.2f)", tTijolos, tTijolos * 899.9 / 100);
-                    break;
-                default:
-                    break;
+                switch (opMaterial) {
+                    case 1:
+                        tTijolos = ceil((altura * 100. * comprimento * 100.) /  (266.));
+                        printf("%d unidades (R$ %.2f)", tTijolos, (float) tTijolos * 400. / 1000.);
+                        break;
+                    case 2:
+                        tTijolos = ceil((altura * 100 * comprimento * 100) /  (361));
+                        printf("%d unidades (R$ %.2f)", tTijolos, tTijolos * 780.5 / 1000.);
+                        break;
+                    case 3:
+                        tTijolos = ceil((altura * 100 * comprimento * 100) /  (551));
+                        printf("%d unidades (R$ %.2f)", tTijolos, tTijolos * 899.9 / 1000.);
+                        break;
+                    default:
+                        printf("Opcao Invalida!");
+                        break;
+                }
+                printf("\n* fim *");
+            } else {
+                printf("Medidas Invalidas\n");
             }
             break;
         default:
