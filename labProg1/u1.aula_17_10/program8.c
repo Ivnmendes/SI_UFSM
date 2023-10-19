e6ec2efd7885725566c9673a25cde8bee5d706cf
Ivan Mendes <ivanmendes2005@gmail.com>
2023-10-19 13:57:52 -0300

    $ git commit -m "exercicios aula 17/10"

--- labProg1/u1.aula_17_10/program8.c
+++ labProg1/u1.aula_17_10/program8.c
@@ -1,31 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-char *baguncaString (char s1[], char s2[]) {
-    char s3[strlen(s1) + strlen(s2)];
-
-    for (int i = 1, j = 0; i < strlen(s1) - 1; i++, j++)
-    {
-        s3[j] = s1[i];
-        s3[j + 1] = s2[j];
-    }
-    s3[strlen(s1) + strlen(s2)] = '\0';
-    
-    return strdup(s3);
-}
-
-char *restauraString (char s1[], char s2[]) {
-    char s3[strlen(s2)];
-
-    for (int i = 0, j = 0; i < strlen(s2); i++, j+=2)
-    {
-        s3[i] = s1[j];
-    }
-    s3[strlen(s2)] = '\O';
-
-    return strdup(s3);
-}
-
 int main(int argc, char const *argv[])
 {
     char s1[20], s2[20];
